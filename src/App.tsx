2275c3bb80db0c3283b37dcbf0ac358c8103abea
harinivas-28 <harinivasg28704@gmail.com>
2025-05-14 14:05:48 +0530

    Made changes required to the client

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,26 @@ import ContactPage from './pages/contact/page'
 import HomePage from './pages/home/page'
 import WhatWeDoPage from './pages/what-we-do/page'
 import { NotFound } from './components/not-found-page'
+import { Analytics } from "@vercel/analytics/react"
 import './App.css'
 
 function App() {
   return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about-us" element={<AboutUsPage />} />
-        <Route path='/gallery' element={<GalleryPage />} />
-        <Route path="/beacon-of-hope" element={<BeaconOfHopePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/what-we-do" element={<WhatWeDoPage />} />
-        <Route path="/login" element={<NotFound />} />  
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path='/gallery' element={<GalleryPage />} />
+          <Route path="/beacon-of-hope" element={<BeaconOfHopePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/what-we-do" element={<WhatWeDoPage />} />
+          <Route path="/login" element={<NotFound />} />  
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   )
 }
-
 export default App
